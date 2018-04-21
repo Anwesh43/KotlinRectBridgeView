0d75b056ee5b60ec618c8725cf2db7c3286d8a6a
Anwesh43 <anweshthecool0@gmail.com>
2018-04-21 10:26:59 +0530

    adding RectBridgeView class

--- rectbridgeview/src/main/java/ui/anwesome/com/rectbridgeview/RectBridgeView.kt
+++ rectbridgeview/src/main/java/ui/anwesome/com/rectbridgeview/RectBridgeView.kt
@@ -3,3 +3,22 @@ package ui.anwesome.com.rectbridgeview
 /**
  * Created by anweshmishra on 21/04/18.
  */
+
+import android.view.*
+import android.content.*
+import android.graphics.*
+
+class RectBridgeView (ctx : Context) : View(ctx) {
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
