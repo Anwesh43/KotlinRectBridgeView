ceb106116458b796cd8f62c0b81ccb1b9deb5e17
Anwesh43 <anweshthecool0@gmail.com>
2018-04-21 11:49:21 +0530

    adding view in activity

--- app/src/main/java/ui/anwesome/com/kotlinrectbridgeview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinrectbridgeview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinrectbridgeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.rectbridgeview.RectBridgeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        RectBridgeView.create(this)
     }
 }
